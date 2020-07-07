0c6f9c279453dbdd645a86ac4bce15eb2ae06b17
João Henrique <Joao@Mac-mini-de-ahazou.local>
2020-07-06 22:30:54 -0300

    Aula04 Usando java.util.Scanner - Java parte 7: Trabalhando com java.io

--- src/br/com/alura/java/io/teste/TesteLeitura2.java
+++ src/br/com/alura/java/io/teste/TesteLeitura2.java
@@ -2,6 +2,8 @@ package br.com.alura.java.io.teste;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteLeitura2 {
@@ -12,7 +14,25 @@ public class TesteLeitura2 {
 		
 		while(scanner.hasNextLine()) {
 			String linha = scanner.nextLine();
-			System.out.println(linha);
+			//System.out.println(linha);
+			
+			Scanner linhaScanner = new Scanner(linha);
+			linhaScanner.useLocale(Locale.US);
+			linhaScanner.useDelimiter(",");
+			
+			String tipoConta = linhaScanner.next();
+			int agencia = linhaScanner.nextInt();
+			int numero = linhaScanner.nextInt();
+			String titular = linhaScanner.next();
+			double saldo = linhaScanner.nextDouble();
+			
+			System.out.format(new Locale("pt", "BR"), "%s - %04d-%08d, %20s: %08.2f %n", 
+					tipoConta, agencia, numero, titular, saldo);
+			
+			
+//			String[] valores = linha.split(",");
+//			System.out.println(valores[3]);
+			
 		}
 		
 		scanner.close();
